b3f0bd0e279eb3d0f5e690ce2a2735934b91847d
Florin Badita-Nistor <baditaflorin@gmail.com>
2026-05-08 11:38:04 +0300

    test: separate unit and e2e suites

--- vitest.config.ts
+++ vitest.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
   },
   test: {
     environment: "jsdom",
+    exclude: ["**/node_modules/**", "**/.git/**", "tests/e2e/**"],
     setupFiles: ["./src/test/setup.ts"],
   },
 });
