e4acfda36511ef5678dbe37de55756b97c12a23c
Florin Badita-Nistor <baditaflorin@gmail.com>
2026-05-09 13:25:42 +0300

    test: run e2e fixtures serially

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,8 +2,9 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests/e2e",
-  fullyParallel: true,
+  fullyParallel: false,
   reporter: [["list"]],
+  workers: 1,
   use: {
     baseURL:
       process.env.PLAYWRIGHT_BASE_URL ??
